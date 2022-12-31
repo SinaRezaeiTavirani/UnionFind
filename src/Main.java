d75d19db6090d01bda77aa2ce2b618ac295cbb74
sinaezar@gmail.com <sinaezar@gmail.com>
2022-12-31 20:24:51 +0330

    QuickUnionUF was added and implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        QuickFindUF uf1 = new QuickFindUF(10);
+        QuickUnionUF uf2 = new QuickUnionUF(10);
+        System.out.println(uf2.connected(2,2));
+        uf2.union(1,7);
+        uf2.union(2, 8);
+        uf2.union(1,8);
+        System.out.println(uf2.connected(8,7));
     }
 }
